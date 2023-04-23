7a374f180df805aae100cb410c1a535e26ae24cf
Gabe <gabrielarce3@gmail.com>
2023-04-22 22:20:07 -0700

    add shazam api key to ignore files

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -7,7 +7,7 @@ export const shazamCoreApi = createApi({
         prepareHeaders: (headers) => {
             headers.set(
                 "X-RapidAPI-Key",
-                "876b17e9bfmsh6890a9bf61af22fp1a4154jsn56bec804bef7"
+                import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
             );
             return headers;
         },
